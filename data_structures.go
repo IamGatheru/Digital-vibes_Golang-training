82da3056e9888da215a16462abf49798362778da
IamGatheru <davidgatheru490@gmail.com>
2024-08-21 20:46:26 +0300

    data strutures in golang

---
+++ data_structures.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("We are in session two")
+/**
+	var planets [8]string
+	planets[0] = "Mercury"
+	planets[1] = "venus"
+	planets[2] = "Earth"
+	planets[3] = "Mars"
+	planets[4] = "Jupiter"
+	planets[5] = "Saturn"
+	planets[6] = "Uranus"
+	planets[7] = "Neptune"
+
+	fmt.Printf("We have 8 planets %v", planets)
+*/
+
+        //Declaring a slice
+        var planets []string
+        planets=append(planets, "Mercury")
+
+}
